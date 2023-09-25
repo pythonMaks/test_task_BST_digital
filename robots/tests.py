70962e9d55b0874c92583396420b26c61cfa6fd1
pythonMaks <python.maxim@yandex.ru>
2023-09-25 11:23:42 +0300

    task 1

--- robots/tests.py
+++ robots/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from datetime import datetime
+from robots.models import Robot
 
-# Create your tests here.
+class RobotTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = '/create_robot/'
+        self.valid_data = {
+            "model": "R2",
+            "version": "D2",
+            "created": "2022-12-31 23:59:59"
+        }
+    
+    def test_create_robot_success(self):
+        response = self.client.post(self.url, data=self.valid_data, content_type='application/json')
+        self.assertEqual(response.status_code, 201)        
+
+        robot = Robot.objects.last()
+        self.assertEqual(robot.serial, "R2-D2")
+        self.assertEqual(robot.model, "R2")
+        self.assertEqual(robot.version, "D2")
+        self.assertEqual(robot.created, datetime.strptime("2022-12-31 23:59:59", '%Y-%m-%d %H:%M:%S'))
+    
+    def test_create_robot_failure(self):
+        invalid_data = {
+            "model": "R2",
+            "version": "D2",
+        }
+        response = self.client.post(self.url, data=invalid_data, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        
+        invalid_data = {
+            "version": "D2",
+            "created": "2022-12-31 23:59:59"
+        }
+        response = self.client.post(self.url, data=invalid_data, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        
+        invalid_data = {
+            "model": "R2",
+            "created": "2022-12-31 23:59:59"
+        }
+        response = self.client.post(self.url, data=invalid_data, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+#from django.views.decorators.csrf import csrf_exempt
+from .models import Robot
+import json
+from datetime import datetime
 
-# Create your views here.
+#@csrf_exempt
+def create_robot(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            model = data.get('model')
+            version = data.get('version')
+            created_str = data.get('created')
+            
+            
+            if not (model and version and created_str):
+                return JsonResponse({"error": "Invalid data"}, status=400)
+            
+            
+            created = datetime.strptime(created_str, '%Y-%m-%d %H:%M:%S')            
+            
+            serial = f"{model}-{version}"
+            
+            Robot.objects.create(serial=serial, model=model, version=version, created=created)
+            
+            return JsonResponse({"success": True}, status=201)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
